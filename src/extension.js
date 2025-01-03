11a5e8a738b382a77dcd81461b450222caf678e3
TiotBenjy <66011955+TiotBenjy@users.noreply.github.com>
2025-01-03 23:44:02 +0100

    Update ext logo + add network checks in main file

--- src/extension.js
+++ src/extension.js
@@ -4,6 +4,7 @@ import Clutter from "gi://Clutter";
 import * as Main from "resource:///org/gnome/shell/ui/main.js";
 import St from "gi://St";
 import Gio from "gi://Gio";
+import GLib from "gi://GLib";
 import * as PanelMenu from "resource:///org/gnome/shell/ui/panelMenu.js";
 import * as PopupMenu from "resource:///org/gnome/shell/ui/popupMenu.js";
 import * as Dialog from "resource:///org/gnome/shell/ui/dialog.js";
@@ -15,14 +16,26 @@ import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 import * as Proxmox from "./modules/proxmox.js";
 
 export default class ContainersExtension extends Extension {
-    /**
-     * enable is the entry point called by gnome-shell
-     */
-    enable() {
+
+    _init() {
+        //region Variables
+        this._settings = this.getSettings();
+        this._idle = false;
+        this._connected = false;
+        this._network_monitor = Gio.network_monitor_get_default();
+        //endregion
+
+        //region Signals
+        this._network_monitor_connection = this._network_monitor.connect('network-changed', this._onNetworkStateChanged.bind(this));
+        //endregion
+
+        this._checkConnectionState();
+    }
+
+    _initUI() {
         console.log(`enabling ${this.uuid} extension`);
         this._indicator = new PanelMenu.Button(0.0, this.metadata.name, false);
         this.menu = this._indicator.menu;
-        this._settings = this.getSettings();
 
         this._indicator.menu.box.add_style_class_name("containers-extension-menu");
         const hbox = new St.BoxLayout({ style_class: "panel-status-menu-box" });
@@ -46,6 +59,31 @@ export default class ContainersExtension extends Extension {
         this._renderMenu();
     }
 
+    stop() {
+        if (this._timeoutCheckConnectionState) {
+            GLib.source_remove(this._timeoutCheckConnectionState);
+            this._timeoutCheckConnectionState = null;
+        }
+
+        if (this._presence_connection) {
+            this._presence.disconnectSignal(this._presence_connection);
+            this._presence_connection = undefined;
+        }
+
+        if (this._network_monitor_connection) {
+            this._network_monitor.disconnect(this._network_monitor_connection);
+            this._network_monitor_connection = undefined;
+        }
+    }
+
+    /**
+     * enable is the entry point called by gnome-shell
+     */
+    enable() {
+        this._init();
+        this._initUI();
+    }
+
 
     /**
      * disable is called when the main extension menu is closed
@@ -65,6 +103,73 @@ export default class ContainersExtension extends Extension {
 
     }
 
+    _onNetworkStateChanged() {
+        this._checkConnectionState();
+    }
+
+    _checkConnectionState() {
+        this._checkConnectionStateRetries = 3;
+        this._oldConnected = this._connected;
+        this._connected = false;
+
+        this._checkConnectionStateWithRetries(1250);
+    }
+
+    _checkConnectionStateWithRetries(interval) {
+        if (this._timeoutCheckConnectionState) {
+            GLib.source_remove(this._timeoutCheckConnectionState);
+            this._timeoutCheckConnectionState = null;
+        }
+
+        this._timeoutCheckConnectionState = GLib.timeout_add(GLib.PRIORITY_DEFAULT, interval, () => {
+            this._timeoutCheckConnectionState = null;
+            let url = this._settings.get_string("pve-host");
+
+            if (!url.startsWith("https://")) {
+                url = "https://" + url;
+            }
+
+            let address = Gio.NetworkAddress.parse_uri(url, 80);
+            let cancellable = Gio.Cancellable.new();
+            try {
+                this._network_monitor.can_reach_async(address, cancellable, this._asyncReadyCallback.bind(this));
+            } catch (err) {
+                let title = _("Can not connect to %s").format(url);
+                log(title + '\n' + err.message);
+                this._checkConnectionStateRetry();
+            }
+            return false;
+        });
+    }
+
+    _checkConnectionStateRetry() {
+        if (this._checkConnectionStateRetries > 0) {
+            let timeout;
+            if (this._checkConnectionStateRetries == 3)
+                timeout = 10000;
+            else if (this._checkConnectionStateRetries == 2)
+                timeout = 30000;
+            else if (this._checkConnectionStateRetries == 1)
+                timeout = 60000;
+
+            this._checkConnectionStateRetries -= 1;
+            this._checkConnectionStateWithRetries(timeout);
+        }
+    }
+
+    _asyncReadyCallback(nm, res) {
+        try {
+            this._connected = this._network_monitor.can_reach_finish(res);
+        } catch (err) {
+            log(title + '\n' + err.message);
+            this._checkConnectionStateRetry();
+            return;
+        }
+        if (!this._oldConnected && this._connected) {
+
+        }
+    }
+
     async _renderMenu() {
         try {
             this.menu.removeAll();
@@ -74,10 +179,10 @@ export default class ContainersExtension extends Extension {
 
             this.menu.addMenuItem(prefs);
 
-            const test = new PopupMenu.PopupMenuItem("Test");
-            
+            const test = new PopupMenu.PopupMenuItem("Test - 1");
+
             test.connect("activate", () => {
-                
+
                 let formatedToken = "PVEAPIToken=";
                 formatedToken += this._settings.get_string("api-token-id");
                 formatedToken += "=";
@@ -98,9 +203,9 @@ export default class ContainersExtension extends Extension {
                         allowUnsafeSsl: this._settings.get_boolean('allow-unsafe-ssl')
                     }).then((vms) => {
                         for (let [_, containers] of vms) {
-                            for (let container of containers) {
-                                log(`[DEBUG - getAllVms] Container: ${container.name}`);
-                                this.menu.addMenuItem(new ContainerSubMenuItem(container, this._settings));
+                            for (let c of containers) {
+                                log(`[DEBUG - getAllVms] Container: ${c.name}`);
+                                this.menu.addMenuItem(new ContainerSubMenuItem(c, this._settings));
                             }
                         }
                     }).catch((error) => {
@@ -144,10 +249,10 @@ class ContainerSubMenuItem extends PopupMenu.PopupSubMenuMenuItem {
         const pauseBtn = createActionButton(
             () => {
                 if (vm.status.split(" ")[0] === "running") {
-                    container.pause();
+                    vm.pause();
                 }
                 if (vm.status.split(" ")[0] === "paused") {
-                    container.unpause();
+                    vm.unpause();
                 }
             },
             "media-playback-pause-symbolic"
@@ -155,7 +260,7 @@ class ContainerSubMenuItem extends PopupMenu.PopupSubMenuMenuItem {
 
         pauseBtn.toggle_mode = true;
 
-        switch (container.status.split(" ")[0]) {
+        switch (vm.status.split(" ")[0]) {
             case "Exited":
             case "exited":
             case "Created":
@@ -186,23 +291,29 @@ class ContainerSubMenuItem extends PopupMenu.PopupSubMenuMenuItem {
                 this.insert_child_at_index(createIcon("action-unavailable-symbolic", "status-undefined"), 1);
                 break;
         }
+        
         // the element on index 3 is the expander, a spacer that clutter fills with space
         this.insert_child_at_index(restartBtn, 4);
         this.insert_child_at_index(pauseBtn, 4);
 
-        if (settings.extraInfo) {
-            // const info = new PopupMenu.PopupMenuItem(`${container.details()}`);
-            // info.add_style_class_name("container-info");
-            // this.menu.addMenuItem(info);
-            this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-        }
-
-        // this.menu.addAction("Show Logs", () => container.logs());
-        // this.menu.addAction("Watch Top", () => container.watchTop());
-        // this.menu.addAction("Open Shell", () => container.shell());
-        // this.menu.addAction("Watch Statistics", () => container.stats());
+        // this.menu.addAction("Show Logs", () => vm.logs());
+        // this.menu.addAction("Watch Top", () => vm.watchTop());
+        // this.menu.addAction("Open Shell", () => vm.shell());
+        // this.menu.addAction("Watch Statistics", () => vm.stats());
         // the css nth- or last-of-type is probably not implemented in gjs
+
+        const info = new PopupMenu.PopupMenuItem("Loading details...");
+        info.add_style_class_name("container-info");
+        this.menu.addMenuItem(info);
+
+        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
         this.menu.box.get_children().at(-1).add_style_class_name("last-container-menu-item");
+
+        // Fetch and update details asynchronously
+        (async () => {
+            const details = await vm.details(); // Ensure `vm.details()` works with await
+            info.label.text = details; // Update the info menu item with fetched details
+        })();
     }
 }
 
